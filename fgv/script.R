1d3946f8c62dc56116d5e5f7936d145027fc3710
fernando88to <fernando88to@Gmail.com>
2018-04-16 15:04:56 -0300

    mais conteduo

--- fgv/script.R
+++ fgv/script.R
@@ -15,3 +15,42 @@ sqrt(4)
 
 #conciente da divisão
 9%/%4
+
+
+# criar objetos  <- ou =
+
+x <- 0 
+(y<-(3*x-1)^2)
+
+# criar vetores
+#vetor de 1 a 6
+vetor1<-1:6
+vetor1
+
+#função c para adcionar valores
+vetor2 <- c(2,4,5,6,7,1,5,6)
+
+vetor3 <- array(1:6)
+
+#acessar pelo indice
+vetor3[3]
+#acessar um intervalor
+vetor3[3:6]
+
+#criar uma matriz
+M <- matrix(1:6, nrow=2, ncol=3)
+#exibe toda a matrix
+M
+#exibe um elemento da matrix
+M[1,3]
+# toda segunda linha
+M[2, ]
+
+#dataframe guardam  tabelas de dados
+
+
+
+
+
+
+
